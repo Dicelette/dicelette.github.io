e25de2fc27245573e34bb4b2441537ee40d5f5e0
Mara-Li <Mara-Li@users.noreply.github.com>
2024-08-09 07:12:44 +0000

    deploy: 3b1dadce4e67c7d46ec0b56f99ae3ddf6f045659

---
+++ en/assets/js/5e95c892.4e13d421.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdicelette_docs=self.webpackChunkdicelette_docs||[]).push([[647],{56002:(e,s,c)=>{c.r(s),c.d(s,{default:()=>n});c(30758);var r=c(13526),t=c(40697),d=c(82886),u=c(71977),a=c(60044),l=c(86070);function n(e){return(0,l.jsx)(t.e3,{className:(0,r.A)(d.G.wrapper.docsPages),children:(0,l.jsx)(a.A,{children:(0,u.v)(e.route.routes)})})}}}]);
--- en/assets/js/87.80954c8f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdicelette_docs=self.webpackChunkdicelette_docs||[]).push([[87],{9087:(e,c,s)=>{s.r(c)}}]);
