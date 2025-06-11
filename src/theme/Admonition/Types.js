2c3648081a2d9f62e6199d2926e73aa0cc287d05
Mara <mara-li@outlook.fr>
2025-06-11 22:46:46 +0200

    add new admonition & upgrade dep

--- src/theme/Admonition/Types.js
+++ src/theme/Admonition/Types.js
@@ -22,13 +22,47 @@ function Usage(props) {
 function Example(props) {
 	return (
 		<div className="theme-admonition theme-admonition-example admonition_o5H7 example alert--example">
-			<div className="admonitionHeading_FzoX">
+			<div className={`admonitionHeading_FzoX ${props.title?.length > 0 ? 'withTitle' : ''}`}>
 				<span className="admonitionIcon_rXq6">
 					<Icon icon="fluent:drafts-24-regular" />
 				</span>
 				{props.title?.length > 0
 					? props.title
 					: translate({ message: "Exemple" })}
+			</div>
+			<div className="admonitionContent_Knsx">{props.children}</div>
+		</div>
+	);
+}
+
+function Roll(props) {
+	return (
+		<div className={`theme-admonition theme-admonition-roll admonition_o5H7 roll alert--roll ${!props.children ? 'inline' : 'withContent'}`}>
+			<div className={`admonitionHeading_FzoX ${props.title?.length > 0 ? 'withTitle' : ''}`}>
+				<span className="admonitionIcon_rXq6">
+					<Icon icon="mdi:die-d20" />
+				</span>
+				{props.title?.length > 0
+					? props.title
+					: translate({ message: "Roll" })}
+			</div>
+			<div className="admonitionContent_Knsx">{props.children}</div>
+		</div>
+	);
+}
+
+
+
+function TLDR(props) {
+	return (
+		<div className="theme-admonition theme-admonition-tldr admonition_o5H7 tldr alert--tldr">
+			<div className="admonitionHeading_FzoX">
+				<span className="admonitionIcon_rXq6">
+					<Icon icon="fluent:document-sparkle-20-filled" />
+				</span>
+				{props.title?.length > 0
+					? props.title
+					: translate({ message: "TL;DR" })}
 			</div>
 			<div className="admonitionContent_Knsx">{props.children}</div>
 		</div>
@@ -57,6 +91,8 @@ const AdmonitionTypes = {
 	usage: Usage,
 	example: Example,
 	pin: Pin,
+	roll: Roll,
+	tldr: TLDR,
 };
 
 export default AdmonitionTypes;
